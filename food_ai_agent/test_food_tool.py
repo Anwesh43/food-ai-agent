3e7207217a916944ad65922a82a1b743aeeccbc3
Anwesh43 <anweshthecool0@gmail.com>
2026-01-18 21:25:19 +0530

    testing getFoodItem table

--- food_ai_agent/test_food_tool.py
+++ food_ai_agent/test_food_tool.py
@@ -1,5 +1,12 @@
 import time 
 from tools.food_tools import getFoodItem, searchFoodName
 
-response = searchFoodName("Sandwich")
+response = searchFoodName("paneer")
 print(response)
+time.sleep(1)
+print("Getting food items++++++++++++++++++")
+
+for item in response:
+    time.sleep(1)
+    print(f"Getting details for {item["name"]}")
+    print(getFoodItem(item["id"]))
