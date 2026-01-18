bf8e316e515d360fc4ba01555d8dd83773782073
Anwesh43 <anweshthecool0@gmail.com>
2026-01-18 23:13:02 +0530

    adding system_prompt in different file

--- food_ai_agent/agents/food_ai_agent.py
+++ food_ai_agent/agents/food_ai_agent.py
@@ -1,14 +1,15 @@
 from pydantic_ai import Agent 
 from tools.food_tools import getFoodItem, searchFoodName
+from prompts.food_system_prompt import SYSTEM_PROMPT
 
 agent = Agent(
     model="ollama:qwen3:8b",
     tools = [getFoodItem, searchFoodName],
-    system_prompt = "analyse food and the nutrients in it"
+    system_prompt = SYSTEM_PROMPT
 )
 
 async def streamFoodAnalysis(prompt : str):
     async with agent.run_stream(prompt) as result:
         async for token in result.stream_text(delta=True):
             print(token, end="")
-            
+
---
+++ food_ai_agent/prompts/food_system_prompt.py
@@ -0,0 +1,6 @@
+SYSTEM_PROMPT = """
+You are an expert food analysis agent, you will help down breaking the nutrients present in an food item
+- Use searchFoodName to search a food item
+- Use getFoodItem with the food id found above to get nutrients of each item.
+Based on user query, help the user with the final result
+"""
